dcc74e4f410b351c3d10eaa07e8a50f4ff1f21dd
Varun Master <varunmaster95@gmail.com>
2019-10-05 19:23:42 -0400

    adding the animal click but the click is not registering. need to debug

--- app.js
+++ app.js
@@ -5,19 +5,40 @@ function renderButtons() {
 
     for (var i = 0; i < animals.length; i++) {
       var a = $("<button>");
-      a.addClass("movie");
-      a.attr("data-name", animals[i]);
+      a.addClass("animal");
+      a.attr("data-animal", animals[i]);
       a.text(animals[i]);
       $("#buttons-view").append(a);
     }
   }
 
-  $("#add-movie").on("click", function(event) {
+  $(".animal").on("click", function() {
+    console.log("Hello");
+    // var userQUery = $(this).attr("data-animal");
+    // var APIEndPoint = "https://api.giphy.com/v1/gifs/search?api_key=dEWru3fa3EkK5GoZcNHpROm3KoRhgCMv&q=" + userQUery;
+    // console.log(userQUery);
+    // $.ajax({
+    //   url: APIEndPoint,
+    //   method: "GET"
+    // }).then(function(res){
+    //   console.log(res);
+    // });//end api call here
+  });//end animal.click here
+
+  $("#add-animal").on("click", function(event) {
     event.preventDefault(); //preventDefault is telling the browser to not refresh the page after a user adds a movie. when it refreshes the page, the movie that the user put in will disappear
-    var animal = $("#movie-input").val().trim(); 
+    var animal = $("#animal-input").val().trim(); 
     animals.push(animal);
     renderButtons();
+    console.log("hello");
+  });//end add-animal.click here
 
-  });
+
 
   renderButtons();
+
+
+
+/*
+<img src="someURL_s.gif" data-still="someURL_s.gif" data-animate="somrURL.gif" data-state="still" class="gif"> //the "_s" in src and data-still tell the browser that its still until you click on it
+ */
