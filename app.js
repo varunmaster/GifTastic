91a91537078d6b253975b3baafb7659d145a7cf5
Varun Master <varunmaster95@gmail.com>
2019-10-05 23:29:29 -0400

    added the click functionality to animate/un-animate gif. added the img tags with the still and animated versions of gifs. TODO: added the rating at the top of the img, split out in rows of 3, and css -__-

--- app.js
+++ app.js
@@ -13,17 +13,49 @@ function renderButtons() {
   }
 
   $("#buttons-view").on("click",".animal", function() { //must use way of onClick because we are dynamically generating .animal buttons. https://stackoverflow.com/questions/8110934/direct-vs-delegated-jquery-on/8111171#8111171
-    console.log("Hello");
+    //console.log("Hello");
+    $("#animal-view").empty(); //emptying the section that holds the animal gifs
     var userQUery = $(this).attr("data-animal");
-    var APIEndPoint = "https://api.giphy.com/v1/gifs/search?api_key=dEWru3fa3EkK5GoZcNHpROm3KoRhgCMv&q=" + userQUery;
-    console.log(userQUery);
+    var APIEndPoint = "https://api.giphy.com/v1/gifs/search?api_key=dEWru3fa3EkK5GoZcNHpROm3KoRhgCMv&limit=10&q=" + userQUery;
+    //console.log(userQUery);
     $.ajax({
       url: APIEndPoint,
       method: "GET"
     }).then(function(res){
       console.log(res);
+      var result = res.data;
+      for(var i = 0; i < result.length; i++) {
+        //console.log("item " + i + ": " + result[i]);
+        var div = $("<div>");
+        div.attr("data-animal", userQUery);
+        div.addClass("gif");
+        var img = $("<img>");
+        img.attr("src",result[i].images["480w_still"].url); //using the square brackets bc need to reference a number object property
+        img.attr("data-still", result[i].images["480w_still"].url);
+        img.attr("data-animate", result[i].images.downsized.url);
+        img.attr("data-state", "still");
+        img.attr("class", "gif");
+        div.append(img);
+        $("#animal-view").append(div);
+      }
     });//end api call here
   });//end animal.click here
+
+  $("#animal-view").on("click",".gif", function() {
+    
+    var state = $(this).attr("data-state");
+    if(state === "still") {
+      var animate = $(this).attr("data-animate");
+      
+      $(this).attr("src",animate);
+      $(this).attr("data-state","animate")
+    }
+    else if(state === "animate") {
+      var still = $(this).attr("data-still");
+      $(this).attr("src",still);
+      $(this).attr("data-state","still");
+    }
+  });//end trigger-animation clicks
 
   $("#add-animal").on("click", function(event) {
     event.preventDefault(); //preventDefault is telling the browser to not refresh the page after a user adds a movie. when it refreshes the page, the movie that the user put in will disappear
@@ -33,11 +65,7 @@ function renderButtons() {
     console.log("hello");
   });//end add-animal.click here
 
-
-
   renderButtons();
-
-
 
 /*
 <img src="someURL_s.gif" data-still="someURL_s.gif" data-animate="somrURL.gif" data-state="still" class="gif"> //the "_s" in src and data-still tell the browser that its still until you click on it
